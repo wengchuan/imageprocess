6c6cf995fd1353cd0db5808956109eb8d02ca6b4
wengchuan <wengchuankot@gmail.com>
2025-09-12 00:00:56 +0800

    Initial commit

---
+++ src/main/java/com/imageprocess/ImageprocessApplication.java
@@ -0,0 +1,13 @@
+package com.imageprocess;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ImageprocessApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ImageprocessApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/imageprocess/ImageprocessApplicationTests.java
@@ -0,0 +1,13 @@
+package com.imageprocess;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ImageprocessApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
